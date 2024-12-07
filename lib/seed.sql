75044aa3ef4b46833a0b2e1394e504c909740153
Beth Kimani <kbwaceke@gmail.com>
2024-12-07 21:01:14 +0300

    created,inserted and selected database

--- lib/seed.sql
+++ lib/seed.sql
@@ -7,3 +7,68 @@ INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
 (6,"Grinch", 2, "M", "Black", "Grinchy", 1),
 (7,"Wendy", 6, "F", "Blue", "naive", 1),
 (8,null, 20, "M", "black", "aggressive", 0);
+select_all_female_bears_return_name_and_age = """
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    WHERE sex='F';
+"""
+
+select_bears_older_than_4 = """
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    WHERE bears.age > 4;
+"""
+
+select_all_calm_bears = """
+    SELECT
+        bears.name,
+        bears.temperament
+    FROM bears
+    WHERE bears.temperament = 'Calm';
+"""
+
+select_all_alive_bears = """
+    SELECT
+        bears.name,
+        bears.age,
+        bears.alive
+    FROM bears
+    WHERE bears.alive = 1;
+"""
+
+select_oldest_bear = """
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    ORDER BY bears.age DESC
+    LIMIT 1;
+"""
+
+select_bears_with_no_name = """
+    SELECT
+        bears.id,
+        bears.age
+    FROM bears
+    WHERE bears.name IS NULL;
+"""
+
+select_all_bears_sorted_by_age_desc = """
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    ORDER BY bears.age DESC;
+"""
+
+select_all_brown_bears = """
+    SELECT
+        bears.name,
+        bears.color
+    FROM bears
+    WHERE bears.color = 'Brown';
+"""
